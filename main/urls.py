042291b8e27f12770f26e0066f3d650bca9347b2
Robson Cruz <ghost0092@gmail.com>
2018-01-06 16:54:11 -0200

    Added view and template for detailed curso.

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^curso/(?P<pk>\d+)/$', views.curso_detail, name='curso_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Curso
+
 
 def index(request):
     cursos = Curso.objects.all()
     return render(request, 'main/index.html', {'cursos': cursos})
+
+
+def curso_detail(request, pk):
+    curso = get_object_or_404(Curso, pk=pk)
+    return render(request, 'main/curso_detail.html', {'curso': curso})
