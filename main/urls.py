3cb7ccd6193e512068976c6858c7053b5dc098f3
Robson Cruz <ghost0092@gmail.com>
2018-01-06 22:10:15 -0200

    Added IES views

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,20 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^curso/list/$', views.curso_list, name='curso_list'),
-    url(r'^curso/(?P<pk>\d+)/$', views.curso_detail, name='curso_detail'),
+    # curso
+    url(r'^curso/list$', views.CursosListView.as_view(), name='curso_list'),
+    url(r'^curso/(?P<pk>\d+)/$', views.CursosDetailView.as_view(),
+        name='curso_detail'),
     url(r'^curso/new/$', views.curso_new, name='curso_new'),
     url(r'^curso/(?P<pk>\d+)/edit/$', views.curso_edit, name='curso_edit'),
+    # ies
+    url(r'^ies/list/$', views.IESListView.as_view(), name='ies_list'),
+    url(r'^ies/(?P<pk>\d+)/$', views.IESDetailView.as_view(),
+        name='ies_detail'),
+    url(r'^ies/(?P<pk>\d+)/edit/$', views.IESUpdateView.as_view(),
+        name='ies_edit'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,24 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
 from .forms import CursoForm
-from .models import Curso
+from .models import Curso, IntituicaoEnsinoSuperior
+
+
+# views Cursos
+class CursosListView(generic.ListView):
+    template_name = 'main/cursos.html'
+
+    def get_queryset(self):
+        return Curso.objects.all()
+
+
+class CursosDetailView(generic.DetailView):
+    model = Curso
+    template_name = 'main/curso_detail.html'
 
 
 def index(request):
     return render(request, 'main/index.html')
-
-
-def curso_list(request):
-    cursos = Curso.objects.all()
-    return render(request, 'main/curso_list.html', {'cursos': cursos})
-
-
-def curso_detail(request, pk):
-    curso = get_object_or_404(Curso, pk=pk)
-    return render(request, 'main/curso_detail.html', {'curso': curso})
 
 
 def curso_new(request):
@@ -22,7 +26,6 @@ def curso_new(request):
         form = CursoForm(request.POST)
         if form.is_valid():
             curso = form.save(commit=False)
-            # curso.cod_IES = request.IES
             curso.save()
             return redirect('curso_detail', pk=curso.pk)
     else:
@@ -41,3 +44,24 @@ def curso_edit(request, pk):
     else:
         form = CursoForm(instance=curso)
     return render(request, 'main/curso_edit.html', {'form': form})
+
+
+# views IES
+
+class IESListView(generic.ListView):
+    template_name = 'main/ies.html'
+
+    def get_queryset(self):
+        return IntituicaoEnsinoSuperior.objects.all()
+
+
+class IESDetailView(generic.DetailView):
+    model = IntituicaoEnsinoSuperior
+    template_name = 'main/ies_detail.html'
+    context_object_name = 'ies'
+
+
+class IESUpdateView(generic.UpdateView):
+    model = IntituicaoEnsinoSuperior
+    fields = ['nome_IES', 'sigla_IES', 'email_IES']
+    template_name = 'main/ies_update_form.html'
