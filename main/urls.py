05aca858bf14b1b06f8ee10e5d9edceb81d1f33b
Robson Cruz <ghost0092@gmail.com>
2018-01-10 13:49:36 -0200

    General polishing of views. Added delete confiramtion for Curso.

--- main/urls.py
+++ main/urls.py
@@ -6,23 +6,23 @@ app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # URLs curso
-    url(r'^curso/$', views.CursosListView.as_view(), name='curso_list'),
+    url(r'^curso/$', views.CursosListView.as_view(), name='curso-list'),
     url(r'^curso/(?P<pk>\d+)/$', views.CursosDetailView.as_view(),
-        name='curso_detail'),
-    url(r'^curso/add/$', views.curso_new, name='curso_new'),
-    url(r'^curso/(?P<pk>\d+)/edit/$', views.curso_edit, name='curso_edit'),
+        name='curso-detail'),
+    url(r'^curso/add/$', views.curso_new, name='curso-add'),
+    url(r'^curso/(?P<pk>\d+)/edit/$', views.curso_edit, name='curso-edit'),
     url(r'^curso/(?P<pk>\d+)/delete/$', views.CursoDeleteView.as_view(),
-        name='curso_delete'),
+        name='curso-delete'),
 
     # URLs IES
-    url(r'^ies/$', views.IESListView.as_view(), name='ies_list'),
+    url(r'^ies/$', views.IESListView.as_view(), name='ies-list'),
     url(r'^ies/(?P<pk>\d+)/$', views.IESDetailView.as_view(),
-        name='ies_detail'),
+        name='ies-detail'),
     url(r'^ies/(?P<pk>\d+)/edit/$', views.IESUpdateView.as_view(),
-        name='ies_edit'),
+        name='ies-edit'),
     url(r'^ies/(?P<pk>\d+)/delete$', views.IESDeleteView.as_view(),
-        name='ies_delete'),
+        name='ies-delete'),
 
     # URLs Mapa
-    url(r'^mapa/$', views.map_view, name='map_view'),
+    url(r'^mapa/$', views.map_view, name='map-view'),
 ]
